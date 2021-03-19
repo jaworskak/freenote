df5c79a1144dac95bf2c5dc879ecbeb0f5fd590f
Karolina <karolinamartamaj@gmail.com>
2021-03-19 10:58:14 +0100

    themes

--- notesfree/src/MainSection/MainSection.js
+++ notesfree/src/MainSection/MainSection.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './MainSection.css'
+import '../App.css'
+import Section from './Section/Section'
 
 const MainSection = () =>{
     return (
-        <div className="mainsection">main section</div>
+        <div className="mainsection">
+            <Section className="section even"/>
+            <Section className="section odd"/>
+            <Section className="section even active"/>
+        </div>
     )
 }
 
---
+++ notesfree/src/MainSection/Section/Section.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Section.css'
+
+const Section = () =>{
+    return (
+        <div className="section">section</div>
+    )
+}
+
+export default Section
