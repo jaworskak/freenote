b359ea1b0c657e79aa10d12eea4e8317829bcab4
Karolina <karolinamartamaj@gmail.com>
2021-03-23 00:26:38 +0100

    adding buttons

--- notesfree/src/MainSection/Section.js
+++ notesfree/src/MainSection/Section.js
@@ -12,19 +12,19 @@ const Section = (props) =>{
              { props.open===props.section_id &&     
         <div className="section active">
             <div className="section-actv-navbar">
-             {props.section_name}
+              <h3>{props.section_name}</h3>
             </div>
             <div className="section-actv-continer">
             </div>
             <div className="section-actv-footer">
-             <button>Dodaj nowy element do <b>{props.section_name}</b></button> 
-              <button>Dodaj nową sekcję do <b>{props.section_name}</b></button>         
+             <button className="section-actv-add-elem">Dodaj nowy element do <b>{props.section_name}</b></button> 
+              <button className="section-actv-add-sect">Dodaj nową sekcję do <b>{props.section_name}</b></button>         
             </div>
         </div>
             }
             { props.open!==props.section_id &&
             <div className="section"  onClick={() => Toggle(props.section_id,props.section_name)} >
-              <p>{props.section_name}</p>
+              <h3>{props.section_name}</h3>
             </div>           
             }
           </div>
